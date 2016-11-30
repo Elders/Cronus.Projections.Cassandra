dc9429001791c68195f8854867d004f95cb3532e
mynkow <mynkow@gmail.com>
2016-11-30 13:41:58 +0200

    Should work

--- src/Elders.Cronus.Projections.Cassandra/Properties/AssemblyInfo.cs
+++ src/Elders.Cronus.Projections.Cassandra/Properties/AssemblyInfo.cs
@@ -9,10 +9,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyrightAttribute("Copyright © 2016")]
 [assembly: AssemblyVersionAttribute("2.0.0.0")]
 [assembly: AssemblyFileVersionAttribute("2.0.0.0")]
-[assembly: AssemblyInformationalVersionAttribute("2.0.0-beta.1+3.Branch.release-2.0.0.Sha.5be7b4c16f655840a970d20b8029ffd24fb2848b")]
+[assembly: AssemblyInformationalVersionAttribute("2.0.0-beta.2+5.Branch.release-2.0.0.Sha.69aad40cf7616dfb556c4b254552712db54ac792")]
 namespace System {
     internal static class AssemblyVersionInformation {
         internal const string Version = "2.0.0.0";
-        internal const string InformationalVersion = "2.0.0-beta.1+3.Branch.release-2.0.0.Sha.5be7b4c16f655840a970d20b8029ffd24fb2848b";
+        internal const string InformationalVersion = "2.0.0-beta.2+5.Branch.release-2.0.0.Sha.69aad40cf7616dfb556c4b254552712db54ac792";
     }
 }
