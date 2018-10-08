b9d0fb1339429b961e3505fc9b9ad8fb26af3760
mynkow <mynkow@gmail.com>
2018-10-08 14:16:57 +0300

    asd

---
+++ src/Elders.Cronus.Projections.Cassandra/ProjectionLoaderDiscovery.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Elders.Cronus.Projections.Cassandra
+{
+    public class ProjectionLoaderDiscovery
+    {
+    }
+}
