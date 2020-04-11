ba9e3a5d1a4303585f2ad084463618cee260f08a
Konstantin Tenekedzhiev <konstantin.tenekedzhiev@eldersoss.com>
2020-04-11 21:42:30 +0300

    DeleteOldProjectionTables defaults to true

--- src/Elders.Cronus.Projections.Cassandra/Infrastructure/TableRetention/TableRetentionOptions.cs
+++ src/Elders.Cronus.Projections.Cassandra/Infrastructure/TableRetention/TableRetentionOptions.cs
@@ -2,7 +2,7 @@
 {
     public class TableRetentionOptions
     {
-        public bool DeleteOldProjectionTables { get; set; } = false;
+        public bool DeleteOldProjectionTables { get; set; } = true;
         public uint NumberOfOldProjectionTablesToRetain { get; set; } = 2;
     }
 }
