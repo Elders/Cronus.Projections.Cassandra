aea0c72fc50f36857ea98963b638db25c463a1ec
Konstantin Tenekedzhiev <konstantin.tenekedzhiev@eldersoss.com>
2020-04-11 18:56:56 +0300

    Unit test for VersionedProjectionsNaming.Parse

--- src/Elders.Cronus.Projections.Cassandra.Tests/When_bulding_snapshot_column_family_from_version.cs
+++ src/Elders.Cronus.Projections.Cassandra.Tests/When_bulding_snapshot_column_family_from_version.cs
@@ -2,16 +2,16 @@
 
 namespace Elders.Cronus.Projections.Cassandra.Tests
 {
-    public class When_bulding_projection_column_family_from_version
+    public class When_bulding_snapshot_column_family_from_version
     {
         Establish context = () =>
         {
             version = new ProjectionVersion("projName", ProjectionStatus.Live, 2, "hash");
         };
 
-        Because of = () => columnFamily = naming.GetColumnFamily(version);
+        Because of = () => columnFamily = naming.GetSnapshotColumnFamily(version);
 
-        It should_create_column_family = () => columnFamily.ShouldEqual("projname_2_hash");
+        It should_create_column_family = () => columnFamily.ShouldEqual("projname_2_hash_sp");
 
         static VersionedProjectionsNaming naming = new VersionedProjectionsNaming();
         static ProjectionVersion version;
---
+++ src/Elders.Cronus.Projections.Cassandra.Tests/When_parsing_version_from_column_family.cs
@@ -0,0 +1,32 @@
+﻿using Machine.Specifications;
+
+namespace Elders.Cronus.Projections.Cassandra.Tests
+{
+    public class When_parsing_version_from_column_family
+    {
+        Establish context = () =>
+        {
+            projectionName = "projname";
+            revision = 2;
+            hash = "hash";
+            columnFamily = $"{projectionName}_{revision}_{hash}";
+        };
+
+        Because of = () => version = naming.Parse(columnFamily);
+
+        It should_parse_version = () => version.ShouldNotBeNull();
+
+        It should_parse_projection_name = () => version.ProjectionName.ShouldEqual(projectionName);
+
+        It should_parse_revision = () => version.Revision.ShouldEqual(revision);
+
+        It should_parse_hash = () => version.Hash.ShouldEqual(hash);
+
+        static VersionedProjectionsNaming naming = new VersionedProjectionsNaming();
+        static ProjectionVersion version;
+        static string columnFamily;
+        static string projectionName;
+        static int revision;
+        static string hash;
+    }
+}
