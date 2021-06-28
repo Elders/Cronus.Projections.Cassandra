52c33c2769beccd20118c4669ca9f96ff387831c
mynkow <mynkow@gmail.com>
2021-06-28 14:49:59 +0300

    fix: Removes locks

--- src/Elders.Cronus.Projections.Cassandra/CassandraProjectionStoreSchema.cs
+++ src/Elders.Cronus.Projections.Cassandra/CassandraProjectionStoreSchema.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Linq;
 using Cassandra;
-using Elders.Cronus.AtomicAction;
 using Elders.Cronus.Projections.Cassandra.Infrastructure;
 using Microsoft.Extensions.Logging;
 
@@ -15,8 +14,6 @@ namespace Elders.Cronus.Projections.Cassandra
         const string DropQueryTemplate = @"DROP TABLE IF EXISTS ""{0}"";";
 
         readonly ISession sessionForSchemaChanges;
-        readonly ILock @lock;
-        private readonly TimeSpan lockTtl;
 
         /// <summary>
         /// Used for cassandra schema changes exclusively
@@ -24,59 +21,27 @@ namespace Elders.Cronus.Projections.Cassandra
         /// https://issues.apache.org/jira/browse/CASSANDRA-11429
         /// </summary>
         /// <param name="sessionForSchemaChanges"></param>
-        public CassandraProjectionStoreSchema(ICassandraProvider cassandraProvider, ILock @lock)
+        public CassandraProjectionStoreSchema(ICassandraProvider cassandraProvider)
         {
             if (ReferenceEquals(null, cassandraProvider)) throw new ArgumentNullException(nameof(cassandraProvider));
-            if (ReferenceEquals(null, @lock)) throw new ArgumentNullException(nameof(@lock));
 
             this.sessionForSchemaChanges = cassandraProvider.GetSession();
-            this.@lock = @lock;
-            this.lockTtl = TimeSpan.FromSeconds(2);
-            if (lockTtl == TimeSpan.Zero) throw new ArgumentException("Lock ttl must be more than 0", nameof(lockTtl));
         }
 
         public string Keyspace => sessionForSchemaChanges.Keyspace;
 
         public void DropTable(string location)
         {
-            if (@lock.Lock(location, lockTtl))
-            {
-                try
-                {
-                    var query = string.Format(DropQueryTemplate, location);
-                    sessionForSchemaChanges.Execute(query, ConsistencyLevel.All);
-                }
-                finally
-                {
-                    @lock.Unlock(location);
-                }
-            }
-            else
-            {
-                logger.Debug($"[Projections] Could not acquire lock for `{location}` to drop projections table");
-            }
+            var query = string.Format(DropQueryTemplate, location);
+            sessionForSchemaChanges.Execute(query, ConsistencyLevel.All);
         }
 
         public void CreateTable(string location)
         {
-            if (@lock.Lock(location, lockTtl))
-            {
-                try
-                {
-                    logger.Debug(() => $"[Projections] Creating table `{location}` with `{sessionForSchemaChanges.Cluster.AllHosts().First().Address}`...");
-                    var query = string.Format(CreateProjectionEventsTableTemplate, location);
-                    var result = sessionForSchemaChanges.Execute(query, ConsistencyLevel.All);
-                    logger.Debug(() => $"[Projections] Created table `{location}`... Maybe?!");
-                }
-                finally
-                {
-                    @lock.Unlock(location);
-                }
-            }
-            else
-            {
-                logger.Warn($"[Projections] Could not acquire lock for `{location}` to create projections table");
-            }
+            logger.Debug(() => $"[Projections] Creating table `{location}` with `{sessionForSchemaChanges.Cluster.AllHosts().First().Address}`...");
+            var query = string.Format(CreateProjectionEventsTableTemplate, location);
+            var result = sessionForSchemaChanges.Execute(query, ConsistencyLevel.All);
+            logger.Debug(() => $"[Projections] Created table `{location}`... Maybe?!");
         }
 
         public void CreateProjectionsStorage(string location)
