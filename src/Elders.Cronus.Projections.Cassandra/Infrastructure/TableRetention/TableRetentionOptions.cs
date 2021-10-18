19bddea95c801597a121f3797eb8328ac6ffe738
gshukov98 <56069656+gshukov98@users.noreply.github.com>
2021-10-18 15:09:20 +0300

    fix: Changes default value of DeleteOldProjectionTables to false

--- src/Elders.Cronus.Projections.Cassandra/Infrastructure/TableRetention/TableRetentionOptions.cs
+++ src/Elders.Cronus.Projections.Cassandra/Infrastructure/TableRetention/TableRetentionOptions.cs
@@ -2,7 +2,7 @@
 {
     public class TableRetentionOptions
     {
-        public bool DeleteOldProjectionTables { get; set; } = true;// This is an experimental feature. It was created for Cosmos DB because M$ charges per table. Till then we will not enable it
+        public bool DeleteOldProjectionTables { get; set; } = false;// This is an experimental feature. It was created for Cosmos DB because M$ charges per table. Till then we will not enable it
         public uint NumberOfOldProjectionTablesToRetain { get; set; } = 2;
     }
 }
